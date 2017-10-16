3a0dc45666ed7d3f98f0f31f54f09f3a9886ef5b
gyh <cngyh96@gmail.com>
2017-10-16 20:28:26 +0800

    mix errors into origin corpus
    update data_utils.py:
    - clean corpus before tokenizing, see details in script
    - change input from txt files into gzip files
    - TODO: pack target file and vocab file into gzip in script

--- data/README.md
+++ data/README.md
@@ -2,3 +2,13 @@
 
 ## 语料库
 国家语委现代汉语语料库，大概五十万条句子。
+
+## 处理过程
+
+1. 按照语料库中字出现频率编码，编码从0开始。用得到的字编码编码原始语料库。
+   在处理过程中会对原始语料库进行清洗，删除空行以及无中文行，替换原始语料库。
+   `python data_utils.py <corpus file path> <encode corpus path> <vocab list file path> <vacab pkl path>`
+
+2. 在原始语料库中加入三种错误：漏字(0)，多字(1)，错字(2)。
+   `python mixerror.py [corpus file path] [vocab pkl path]`
+
--- data/data_utils.py
+++ data/data_utils.py
@@ -50,6 +50,39 @@ GO_ID = 1
 EOS_ID = 2
 UNK_ID = 3
 
+def clean_corpus(data_path, corpus_data):
+    """ Clean corpus data to delete empty lines
+        or line without Chinese characters.
+        If the corpus is unclean, replace origin corpus_data with new data.
+        Args:
+            data path: origin corpus file path
+            corpus_data: corpus data lists
+        Returns:
+            new_corpus_data: corpus data whether the corpus is clean or not.
+    """
+    new_corpus_data = []
+    for sentence in corpus_data:
+        if len(sentence) == 0:
+            # empty line
+            continue
+        else:
+            have_ch_character = ['\u4e00' <= c <= '\u9fff' for c in sentence]
+            if True not in have_ch_character:
+                # no chinese character
+                continue
+            else:
+                new_corpus_data.append(sentence)
+    if len(new_corpus_data) != len(corpus_data):
+        # unclean, recreat corpus data
+        data = '\n'.join(new_corpus_data)
+        data = gzip.compress(data.encode())
+        with open(data_path, 'wb') as f:
+            f.write(data)
+        print('clean origin corpus: {}'.format(data_path))
+    else:
+        print('origin corpus is clean')
+    return new_corpus_data
+
 def basic_tokenizer(sentence):
     """Very basic tokenizer: split the sentence into a list of tokens."""
     words = []
@@ -80,34 +113,35 @@ def create_vocabulary(vocabulary_path, data_path, max_vocabulary_size, tokenizer
     normalize_char: Boolean; if true, all characters are replaced by a.
     """
     if not os.path.exists(vocabulary_path):
+        corpus_data = gzip.GzipFile(data_path, mode='r').read().decode().split('\n')
+        corpus_data = clean_corpus(data_path, corpus_data)
         print("Creating vocabulary %s from data %s" % (vocabulary_path, data_path))
         vocab = {}
-        with open(data_path, "r", encoding='UTF-8') as f:
-            counter = 0
-            for line in f:
-                counter += 1
-                if counter % 100000 == 0:
-                    print("  processing line %d" % counter)
-                tokens = tokenizer(line) if tokenizer else basic_tokenizer(line)
-                for w in tokens:
-                    word = _DIGIT_RE.sub("0", w) if normalize_digits else w
-                    word = _CHAR_RE.sub("a", w) if normalize_char else w
-                    word = '0' if normalize_digits and (word=='０' or word=='１' or word=='２' or word=='３' or word=='４' or word=='５' or word=='６' or word=='７' or word=='８' or word=='９') else w
-                    if word in vocab:
-                        vocab[word] += 1
-                    else:
-                        vocab[word] = 1
-            vocab_list = _START_VOCAB + sorted(vocab, key=vocab.get, reverse=True)
-            #print(vocab)
-            if len(vocab_list) > max_vocabulary_size:    
-                vocab_list = vocab_list[:max_vocabulary_size]
-            with open(pickle_path, 'wb') as f:
-                pickle.dump(vocab_list, f)
-            with open(vocabulary_path, mode="w", encoding='UTF-8') as vocab_file:
-                for w in vocab_list:
-                    vocab_file.write(w + "\n")
 
-                    
+        counter = 0
+        for line in corpus_data:
+            counter += 1
+            if counter % 100000 == 0:
+                print("  processing line %d" % counter)
+            tokens = tokenizer(line) if tokenizer else basic_tokenizer(line)
+            for w in tokens:
+                word = _DIGIT_RE.sub("0", w) if normalize_digits else w
+                word = _CHAR_RE.sub("a", w) if normalize_char else w
+                word = '0' if normalize_digits and (word=='０' or word=='１' or word=='２' or word=='３' or word=='４' or word=='５' or word=='６' or word=='７' or word=='８' or word=='９') else w
+                if word in vocab:
+                    vocab[word] += 1
+                else:
+                    vocab[word] = 1
+        vocab_list = _START_VOCAB + sorted(vocab, key=vocab.get, reverse=True)
+        #print(vocab)
+        if len(vocab_list) > max_vocabulary_size:
+            vocab_list = vocab_list[:max_vocabulary_size]
+        with open(pickle_path, 'wb') as f:
+            pickle.dump(vocab_list, f)
+        with open(vocabulary_path, mode="w", encoding='UTF-8') as vocab_file:
+            for w in vocab_list:
+                vocab_file.write(w + "\n")
+
 def initialize_vocabulary(vocabulary_path):
     """Initialize vocabulary from file.
 
@@ -188,16 +222,17 @@ def data_to_token_ids(data_path, target_path, vocabulary_path,
     if not os.path.exists(target_path):
         print("Tokenizing data in %s" % data_path)
         vocab, _ = initialize_vocabulary(vocabulary_path)
-        with open(data_path, mode="r", encoding='UTF-8') as data_file:
-            with open(target_path, mode="w", encoding='UTF-8') as tokens_file:
-                counter = 0
-                for line in data_file:
-                    counter += 1
-                    if counter % 100000 == 0:
-                        print("  tokenizing line %d" % counter)
-                    token_ids = sentence_to_token_ids(line, vocab, tokenizer,
-                                            normalize_digits, normalize_char)
-                    tokens_file.write(" ".join([str(tok) for tok in token_ids]) + "\n")
+
+        corpus_data = gzip.GzipFile(data_path, mode='r').read().decode().split('\n')
+        with open(target_path, mode="w", encoding='UTF-8') as tokens_file:
+            counter = 0
+            for line in corpus_data:
+                counter += 1
+                if counter % 100000 == 0:
+                    print("  tokenizing line %d" % counter)
+                token_ids = sentence_to_token_ids(line, vocab, tokenizer,
+                                        normalize_digits, normalize_char)
+                tokens_file.write(" ".join([str(tok) for tok in token_ids]) + "\n")
 
 max_vocabulary_size = 12000
 create_vocabulary(vocabulary_path, data_path, max_vocabulary_size)
